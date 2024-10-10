764dcdfe882fe90ccd0079bd2f5ab12ae90a562c
Manyaka <104553245+237Codes@users.noreply.github.com>
2024-10-09 23:47:51 -0400

    adding the react icons library and building nav bar buttons

--- frontend/vite-project/src/components/Navbar.jsx
+++ frontend/vite-project/src/components/Navbar.jsx
@@ -13,11 +13,24 @@ const Navbar = () => {
         }}
         >
           <Text
-            
+            fontSize={{ base: "22", sm:"28"}}
+            fontWeight={"bold"}
+            textTransform={"uppercase"}
+            textAlign="center"
+            bgGradient="linear(to-r, cyan.400, blue.500, purple.600)"
+            bgClip={"text"}
           >
-
+            <Link to ={"/"}>Travel Memories ✈️ </Link>
           </Text>
-        </Flex>
+
+          <HStack spacing={2} alignItems={"center"}>
+            <Link to ={"/create"}>
+            <Button>
+              <PlusSquareIcon/>
+            </Button>
+            </Link>
+          </HStack>
+      </Flex>
 
     </Container>;
 };
