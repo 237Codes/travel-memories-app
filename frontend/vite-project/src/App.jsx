667fe12dd5cea71b43fd0050eef977f305d8deaa
Manyaka <104553245+237Codes@users.noreply.github.com>
2024-10-10 01:00:35 -0400

    fix route to navigation

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -2,7 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import { Box } from "@chakra-ui/react";
 import CreatePage from "./pages/CreatePage";
 import HomePage from "./pages/HomePage";
-import Navbar from "./components/Navbar/Navbar";
+import Navbar from "./components/Navbar";
 
 function App() {
 
