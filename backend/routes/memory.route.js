78a95491ec5416860cebc7cbe4df708f9be8da56
Manyaka <104553245+237Codes@users.noreply.github.com>
2024-10-26 00:14:31 -0400

    fixing api routes for post method

--- backend/routes/memory.route.js
+++ backend/routes/memory.route.js
@@ -7,8 +7,8 @@ import { deleteMemory } from "../controllers/memory.controller.js";
 
 const router  = express.Router();
 
-router.get("", getMemory);
-router.post("", createMemory);
+router.get("/", getMemory);
+router.post("/", createMemory);
 router.put("/:id", updateMemory);
 console.log(process.env.MONGO_URI); //Connecting the database
 router.delete("/:id", deleteMemory )
