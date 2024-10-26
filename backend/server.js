8ec885b4ea82d0a3cc55e49dcc959dbd8127914a
Manyaka <104553245+237Codes@users.noreply.github.com>
2024-10-25 23:55:41 -0400

    fixing api endpoint for fetching a memory

--- backend/server.js
+++ backend/server.js
@@ -7,13 +7,13 @@ import memoryRoutes from "./routes/memory.route.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 8000 
+const PORT = process.env.PORT || 8000; 
 
 app.use(express.json()); //allows us to parse or accept json data in the reuest.body
 
 app.use("/api/memories", memoryRoutes); // This will call the functions inside the memory.routes.js file
 
-app.listen(8000, () => { 
+app.listen(PORT, () => { 
     connectDB();// as soon as we listen to the app, let us connect to the database
     console.log("Server started on  http://localhost:" + PORT);
 });
