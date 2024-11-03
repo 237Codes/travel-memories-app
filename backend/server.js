13538bd72d42c200d763fee1660ac593f385d40d
Manyaka <104553245+237Codes@users.noreply.github.com>
2024-11-03 13:20:55 -0500

    make frontend static assets so server serves both frontend and back end

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 
+// for config for deployment
+import path from "path";
+
 //import cors module
 import cors from "cors";
 
@@ -11,10 +14,18 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8000; 
+const __dirname = path.resolve()
 
 app.use(express.json()); //allows us to parse or accept json data in the reuest.body
 app.use(cors({ origin: 'http://localhost:5173' }));
 app.use("/api/memories", memoryRoutes); // This will call the functions inside the memory.routes.js file
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    })
+}
 
 app.listen(PORT, () => { 
     connectDB();// as soon as we listen to the app, let us connect to the database
